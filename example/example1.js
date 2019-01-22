ee7b780aaeb62b5fc3eac5fe0b400308ceff1552
Valery Baranov <valery@baranov.xyz>
2019-01-22 14:08:42 +0300

    Refactor to ES6 syntax

--- example/example1.js
+++ example/example1.js
@@ -10,12 +10,15 @@
 'use strict'
 
 const fs = require('fs')
+const path = require('path')
 const XlsxStreamReader = require('../')
 
 var workBookReader = new XlsxStreamReader()
 workBookReader.on('error', function (error) {
-  throw (error)
+  throw error
 })
+
+var rows = []
 
 workBookReader.on('worksheet', function (workSheetReader) {
   if (workSheetReader.id > 1) {
@@ -26,14 +29,19 @@ workBookReader.on('worksheet', function (workSheetReader) {
   }
   console.log('Worksheet:', workSheetReader.id)
 
+  let c = 0
   workSheetReader.on('row', function (row) {
-    row.values.forEach(function (rowVal, colNum) {
-      console.log('RowNum', row.attributes.r, 'colNum', colNum, 'rowValLen', rowVal.length, 'rowVal', "'" + rowVal + "'")
-    })
+    c++
+    if (c % 100 === 0) console.log(c)
+    rows.push(row.values)
+    // row.values.forEach(function (rowVal, colNum) {
+    //   console.log('RowNum', row.attributes.r, 'colNum', colNum, 'rowValLen', rowVal.length, 'rowVal', "'" + rowVal + "'")
+    // })
   })
 
   workSheetReader.on('end', function () {
-    console.log('Worksheet', workSheetReader.id, 'rowCount:', workSheetReader.rowCount)
+    // console.log('Worksheet', workSheetReader.id, 'rowCount:', workSheetReader.rowCount)
+    console.log(`Sheet length: ${rows.length} rows.`)
   })
 
   // call process after registering handlers
@@ -43,4 +51,7 @@ workBookReader.on('end', function () {
   console.log('finished!')
 })
 
-fs.createReadStream('example/example1.xlsx').pipe(workBookReader)
+// fs.createReadStream('example/example1.xlsx').pipe(workBookReader)
+const filePath = path.join(__dirname, '..', '..', 'busher_eq.zip')
+console.log(filePath)
+fs.createReadStream(filePath).pipe(workBookReader)
